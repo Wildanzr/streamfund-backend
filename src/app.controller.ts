cc29462464708254a773621f4311d0038d3615c1
Danzrrr <graita.sukma@gmail.com>
2024-10-06 09:48:02 +0700

    feat: base config

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+class Test {
+  @ApiProperty({
+    required: true,
+    type: String,
+    description: 'Nama',
+  })
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+}
 
 @Controller()
 export class AppController {
@@ -8,5 +21,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('/test')
+  getTest(@Body() body: Test): Test {
+    return body;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,54 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('Main');
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+    logger: ['error', 'debug', 'log', 'error', 'warn'],
+  });
+
+  // ValidationPipe
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      forbidNonWhitelisted: true,
+      forbidUnknownValues: true,
+    }),
+  );
+
+  // CORS
+  const options: CorsOptions = {
+    origin:
+      process.env.NODE_ENV === 'production' ? process.env.CORS_ORIGIN : '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  };
+  app.enableCors(options);
+
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Streamfund Backend')
+    .setDescription('Streamfund API Documentation')
+    .setVersion('0.1.0')
+    .addTag('streamfund')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  // Helmet
+  app.use(helmet());
+
+  const PORT = process.env.PORT || 5111;
+
+  await app.listen(PORT, () => {
+    logger.log(`Server running on http://localhost:${PORT}`);
+  });
 }
 bootstrap();
